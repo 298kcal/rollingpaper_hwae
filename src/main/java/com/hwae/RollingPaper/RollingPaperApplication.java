26ae2dedebe9ccd195efd47408c2e05098e83339
Egene Chung <90591897+298kcal@users.noreply.github.com>
2022-08-14 22:31:25 +0900

    Add files via upload

---
+++ src/main/java/com/hwae/RollingPaper/RollingPaperApplication.java
@@ -0,0 +1,13 @@
+package com.hwae.RollingPaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingPaperApplication.class, args);
+	}
+
+}
